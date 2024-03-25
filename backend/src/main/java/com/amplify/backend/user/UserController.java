2c89332212dc9035bb771df2830642e2cdd53b70
NicholasLe04 <Nicholasl.le224@gmail.com>
2024-03-25 11:21:20 -0700

    Implement Email Exists check

--- backend/src/main/java/com/amplify/backend/user/UserController.java
+++ backend/src/main/java/com/amplify/backend/user/UserController.java
@@ -3,14 +3,16 @@ package com.amplify.backend.user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/users")
 public class UserController {
 
     private final UserService userService;
@@ -28,5 +30,15 @@ public class UserController {
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUserByEmail(@PathVariable Long id) {
+        userService.deleteUserById(id);
     }
 }
--- backend/src/main/java/com/amplify/backend/user/UserService.java
+++ backend/src/main/java/com/amplify/backend/user/UserService.java
@@ -1,6 +1,7 @@
 package com.amplify.backend.user;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,7 +21,20 @@ public class UserService {
     }
 
     public User createUser(User user) {
+        Optional<User> userOptional = userRepository.findByEmail(user.getEmail());
+        if (userOptional.isPresent()) {
+            throw new IllegalStateException("Email already taken");
+        }
         return userRepository.save(user);
     }
 
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException("User with id " + id + " not found"));
+    }
+
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+        System.out.println("User with id " + id + " deleted");
+    }
 }
