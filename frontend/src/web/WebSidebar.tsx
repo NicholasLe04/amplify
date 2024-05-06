e992a2acaf799d5af31d44a5d7e3f8bb6d169e15
Alex Bui <alexnbui@gmail.com>
2024-05-06 03:56:57 -0700

    add now playing display

--- frontend/src/web/WebSidebar.tsx
+++ frontend/src/web/WebSidebar.tsx
@@ -1,7 +1,27 @@
 import { Link } from 'react-router-dom'
-import { GrHomeRounded, GrSearch, GrUser } from "react-icons/gr"
+import { GrHomeRounded, GrSearch, GrUser } from 'react-icons/gr'
+import { useQuery } from '@tanstack/react-query'
 
 export default function WebSidebar() {
+
+    const nowPlaying = useQuery({
+        queryKey: ['nowPlaying'],
+        queryFn: async () => {
+            const token = localStorage.getItem('access_token')
+            const res = await fetch(
+                'https://api.spotify.com/v1/me/player/currently-playing',
+                {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                }
+            )
+            const data = await res.json()
+            return data
+        },
+        refetchInterval: 1000
+    })
 
     return (
         <>
@@ -17,15 +37,22 @@ export default function WebSidebar() {
                     </div>
                 </div>
                 <div className='flex flex-col gap-3'>
-                    <div>
-                        now playing
-                    </div>
+                    {nowPlaying.isSuccess ?
+                        < div className='flex flex-col gap-3'>
+                            <img className='' src={nowPlaying.data.item?.album.images[0].url} />
+                            <div>
+                                <div className='truncate'>{nowPlaying.data.item?.name}</div>
+                                <div className='truncate text-gray-400'>{nowPlaying.data.item?.artists.map((artist: any) => { return artist.name }).join(', ')}</div>
+                            </div>
+                        </div>
+                        : 'loading'
+                    }
                     <Link className='flex p-5 gap-5 bg-space-light rounded-xl' to='/profile'>
                         <GrUser className='my-auto' />
                         <div className='flex-1 text-lg'>Profile</div>
                     </Link>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
