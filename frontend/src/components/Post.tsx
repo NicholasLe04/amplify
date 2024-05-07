944cf389612645432df802de513d03d999061c61
NicholasLe04 <Nicholasl.le224@gmail.com>
2024-05-07 14:39:30 -0700

    Add time posted for posts

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -31,13 +31,23 @@ export default function Post({post}: Props) {
         return spotifyUrl.split('/')[3];
     }
 
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
     const postType = getPostType(post.spotifyUrl);
+    const date = new Date(post.postedAt);
+    const day = date.getDate();
+    const monthIndex = date.getMonth();
+    const monthName = months[monthIndex];
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const time = `${hours % 12 || 12}:${minutes < 10 ? '0' : ''}${minutes} ${hours < 12 ? 'AM' : 'PM'}`;
 
     return (
         <div key={post.id} className="bg-space-light rounded-lg p-6 mb-4">
             <div className="flex items-center mb-2">
                 <img className="rounded-full" width={32} src={post.author.imgUrl} />
                 <p className="text-white ml-2">{post.author.displayName} is sharing {postType === 'artist' ? 'an' : 'a'} {postType}</p>
+                <p className="text-sm text-gray-400 ml-2">{monthName} {day} at {time}</p>
             </div>
             <p className="text-sm text-white font-thin tracking-wider mb-4">{post.description}</p>
             <iframe
